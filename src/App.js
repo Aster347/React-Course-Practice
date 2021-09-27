06dfea778c4f5394468b5436a1bcd7a12f8a7837
Ethan Maxwell <e.maxwell18p@gmail.com>
2021-09-27 12:59:28 -0400

    Practice app done, errors and list funtional

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import AddUser from './components/Users/AddUser';
+import { useState } from "react";
+import AddUser from "./components/Users/AddUser";
+import UsersList from "./components/Users/UsersList";
 
 function App() {
+  const [usersList, setUsersList] = useState([]);
+
+  const addUserHandler = (uName, uAge) => {
+    setUsersList((prevUserList) => {
+      return [
+        ...prevUserList,
+        { name: uName, age: uAge, id: Math.random().toString() },
+      ];
+    });
+  };
+
   return (
     <div>
-      <AddUser />
+      <AddUser onAddUser={addUserHandler} />
+      <UsersList users={usersList} />
     </div>
   );
 }
